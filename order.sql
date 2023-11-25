54c181c90486123f59912c2e5963668bb713d235
punamsatpute <143501077+punamsatpute@users.noreply.github.com>
2023-11-25 03:00:12 -0800

    Add files via upload

---
+++ order.sql
@@ -0,0 +1,35 @@
+create database lab3;
+use lab3;
+CREATE TABLE orders (
+    ord_no INT PRIMARY KEY,
+    purch_amt DECIMAL(10, 2),
+    ord_date DATE,
+    customer_id INT,
+    salesman_id INT
+);
+
+INSERT INTO orders VALUES
+(70001, 150.5, '2012-10-05', 3005, 5002),
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.5, '2012-08-17', 3009, 5003),
+(70007, 948.5, '2012-09-10', 3005, 5002),
+(70005, 2400.6, '2012-07-27', 3007, 5001),
+(70008, 5760, '2012-09-10', 3002, 5001),
+(70010, 1983.43, '2012-10-10', 3004, 5006),
+(70003, 2480.4, '2012-10-10', 3009, 5003),
+(70012, 250.45, '2012-06-27', 3008, 5002),
+(70011, 75.29, '2012-08-17', 3003, 5007),
+(70013, 3045.6, '2012-04-25', 3002, 5001);
+
+SELECT SUM(purch_amt) AS total_purchase_amount FROM orders;
+
+SELECT AVG(purch_amt) AS average_purchase_amount FROM orders;
+
+SELECT COUNT(DISTINCT salesman_id) AS number_of_salespeople FROM orders;
+
+SELECT COUNT(DISTINCT customer_id) AS number_of_customers FROM orders;
+
+SELECT MAX(purch_amt) AS max_purchase_amount FROM orders;
+
+SELECT MIN(purch_amt) AS min_purchase_amount FROM orders;
